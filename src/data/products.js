62eb8b6de02c40612117fbdb52a5884474f3db25
gonsalvesbliss <blissgonsalves07@gmail.com>
2026-03-30 14:14:53 +0530

    Add files via upload

---
+++ src/data/products.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: 1, name: "Laptop", price: 50000 },
+  { id: 2, name: "Mobile", price: 20000 },
+  { id: 3, name: "Headphones", price: 3000 }
+];
+
+export default products;
